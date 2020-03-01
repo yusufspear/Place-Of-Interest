5022dab5f33fa3b5e20b41143f70e9c3065b992c
yusufspear <33243685+yusufspear@users.noreply.github.com>
2020-03-01 23:08:22 +0530

    Sync Profile Image

--- app/src/main/java/com/example/placeofinterest/Home.java
+++ app/src/main/java/com/example/placeofinterest/Home.java
@@ -3,6 +3,7 @@ package com.example.placeofinterest;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
@@ -14,6 +15,7 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
@@ -26,15 +28,18 @@ import android.os.Looper;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -54,6 +59,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -77,19 +83,23 @@ import static android.widget.Toast.LENGTH_LONG;
 
 public class Home extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final double Lang= 19.077806;
+    private static final double Lat= 19.077806;
     private static final double Long= 72.883056;
     private static final int GPS_REQUEST_CODE =9001;
+    private static final int PROXIMITY_RADIUS = 20000;
     GoogleMap mMap;
     FloatingActionButton mCurrentLocation, mDirection;
     FrameLayout frameLayout;
-//    SearchView searchView;
+    Button rest,atm,hotel;
+    //    SearchView searchView;
     private static String TAG="which";
     BottomNavigationView bottomNavigationView;
     private FusedLocationProviderClient mLocationClient;
     private LocationCallback mLocationCallback;
     HandlerThread handlerThread;
     private View doorView;
+    private String apiKey;
+    private Location deviceCurrentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,7 +124,7 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
 
         supportMapFragment.getMapAsync(this);
         initViews();
-        String apiKey = getString(R.string.google_maps_key);
+        apiKey = getString(R.string.google_maps_key);
 
         if (!Places.isInitialized()) {
             Places.initialize(Home.this, apiKey);
@@ -126,34 +136,37 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
                 getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
         autocompleteSupportFragment.getView().setBackgroundResource(R.drawable.searchfield);
         autocompleteSupportFragment.setActivityMode(AutocompleteActivityMode.OVERLAY);
-        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.TYPES,
-        Place.Field.PHONE_NUMBER, Place.Field.PLUS_CODE,Place.Field.WEBSITE_URI, Place.Field.RATING, Place.Field.USER_RATINGS_TOTAL));
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.NAME, Place.Field.LAT_LNG,Place.Field.ADDRESS,
+        Place.Field.PHONE_NUMBER, Place.Field.WEBSITE_URI, Place.Field.RATING, Place.Field.USER_RATINGS_TOTAL));
 
         autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(@NonNull Place place) {
-                Toast.makeText(getApplicationContext(),"Place: " + place.getName() + ", " + place.getId(), Toast.LENGTH_LONG).show();
+                mMap.clear();
+                Toast.makeText(Home.this,"Place: " + place.getName() + ", " + place.getId(), LENGTH_LONG).show();
                     if (place.getLatLng()!=null){
-                        Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                        Log.i(TAG, "Place: " + place.getName());
 
                         LatLng address = place.getLatLng();
-                        mMap.animateCamera(CameraUpdateFactory.zoomTo(5.0f));
-                        mMap.addMarker(new MarkerOptions().position(address).title(place.getName()).snippet(place.getRating().toString()));
-                        CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(address,15);
+                        mMap.animateCamera(CameraUpdateFactory.zoomTo(4.0f));
+                        Marker marker =mMap.addMarker(new MarkerOptions().position(address).draggable(true).title(place.getName())
+                                .snippet(  "ADDRESS:  "+place.getAddress() +"\nRATING: "+place.getRating()+"\nCONTACT: "+place.getPhoneNumber()));
+                        marker.hideInfoWindow();
+                        mMap.setInfoWindowAdapter(new CustomInfoWindow_Adapter(Home.this));
+                        CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(address,18);
                         mMap.animateCamera(cameraUpdate, 1500, new GoogleMap.CancelableCallback() {
                         @Override
                         public void onFinish() {
-                                Toast.makeText(Home.this, "Finished", Toast.LENGTH_SHORT).show();
                         }
 
                         @Override
                         public void onCancel() {
-                                Toast.makeText(Home.this, "Cancelled", Toast.LENGTH_SHORT).show();
-
                         }
                     });
+
                     }
             }
+
 
             @Override
             public void onError(@NonNull Status status) {
@@ -161,43 +174,21 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
 
             }
         });
-
-
-//        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//        Object[] dataTransfer=new Object[2];
+//        GetNearbyPlacesData getNearbyPlacesData=new GetNearbyPlacesData();
+//        atm.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onPlaceSelected(@NonNull Place place) {
-//                LatLng addres=place.getLatLng();
-//                Log.i("OnPlaceSelect", String.valueOf(place.getLatLng()));
-//                Toast.makeText(Home.this,addres.toString(), LENGTH_LONG).show();
-//                Toast.makeText(Home.this,"Place: " + place.getName() + ", " + place.getId() +
-//                        "\n Latitute" + place.getLatLng().latitude +"\nLongtitute  " + place.getLatLng().longitude,Toast.LENGTH_LONG).show();
-//                autocompleteSupportFragment.getView().setVisibility(View.VISIBLE);
-//                mMap.animateCamera(CameraUpdateFactory.zoomTo(3.0f));
-//                LatLng address = new LatLng(place.getLatLng().latitude, place.getLatLng().longitude);
-//                showMarker(address);
-//                CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(address,19);
-//                mMap.animateCamera(cameraUpdate, 1000, new GoogleMap.CancelableCallback() {
-//                    @Override
-//                    public void onFinish() {
-////                        Toast.makeText(Home.this, "Finished", Toast.LENGTH_SHORT).show();
-//                    }
+//            public void onClick(View view) {
+//                mMap.clear();
+//                String ATM = "Atm";
+//                String url = getUrl(Lat, Long, ATM);
+//                dataTransfer[0] = mMap;
+//                dataTransfer[1] = url;
 //
-//                    @Override
-//                    public void onCancel() {
-////                        Toast.makeText(Home.this, "Cancelled", Toast.LENGTH_SHORT).show();
-//
-//                    }
-//                });
-//
-//            }
-//
-//            @Override
-//            public void onError(@NonNull Status status) {
-//                Log.i("PlaceError", "An error occurred: " + status);
+//                getNearbyPlacesData.execute(dataTransfer);
+//                Toast.makeText(Home.this, "Showing Nearby ATM", LENGTH_LONG).show();
 //            }
 //        });
-
-
 
         mCurrentLocation.setOnClickListener(this::CurrentLocation);
 
@@ -216,12 +207,8 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
                     Log.i("loc", "onLocationResult: Execute");
                     Location address= locationResult.getLastLocation();
                     Toast.makeText(Home.this,"Lat:- " +address.getLatitude()+"\n Long:- "+address.getLongitude(), LENGTH_LONG).show();
-
                     LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-//                    showMarker(latLng);
                     mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
-
-
 
                 });
 
@@ -321,14 +308,26 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
 
     }
 
-
-
+//    private String getUrl(double latitude , double longitude , String nearbyPlace) {
+//
+//        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%2B61293744000&inputtype=phonenumber&fields=place_id");
+////        googlePlaceUrl.append("location="+"19.080690,72.883062");
+////        googlePlaceUrl.append("&radius="+5000);
+////        googlePlaceUrl.append("&type="+"atm");
+////        googlePlaceUrl.append("&sensor=true");
+//        googlePlaceUrl.append("&key="+apiKey);
+//
+//        Log.d("which", "url = "+googlePlaceUrl.toString());
+//
+//        return googlePlaceUrl.toString();
+//    }
     private void CurrentLocation(View view) {
 
 
         if (isGPSEnabled()){
 
             mLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+
                 @Override
                 public void onComplete(@NonNull Task<Location> task) {
                     if (task.isSuccessful()){
@@ -336,6 +335,7 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
                         Location address = task.getResult();
                         mMap.animateCamera(CameraUpdateFactory.zoomTo(3.0f));
                         LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
+                        deviceCurrentLocation=address;
                         showMarker(latLng);
 
                         CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(latLng,19);
@@ -407,6 +407,9 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
         frameLayout = findViewById(R.id.map_fragment);
         mCurrentLocation = findViewById(R.id.fab_myLocation);
         mDirection = findViewById(R.id.fab_direction);
+        rest=findViewById(R.id.restaurant);
+        atm=findViewById(R.id.atm);
+        hotel=findViewById(R.id.hotel);
 //        searchView = findViewById(R.id.search_View);
 
 
--- app/src/main/java/com/example/placeofinterest/Profile.java
+++ app/src/main/java/com/example/placeofinterest/Profile.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,10 +21,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,7 +38,10 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -66,11 +74,20 @@ public class Profile extends AppCompatActivity {
         uploadImageButton.setOnClickListener(this::UploadImage);
 
         mAuth=FirebaseAuth.getInstance();
-//        FirebaseUser user=mAuth.getCurrentUser();
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
         String[] New = new String[6];
         mStorageRef = FirebaseStorage.getInstance().getReference().child("Profile Image");
         DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("User");
+                if (user!=null){
+            Log.i("which", "User NOT NULL ");
+            if (user.getPhotoUrl()!=null){
+                Log.i("which", "PhotoUrl was there: ");
+                Glide.with(Profile.this)
+                        .load(user.getPhotoUrl())
+                        .into(userphoto);
+            }
 
+        }
         mRef.child(mAuth.getCurrentUser().getUid())
                 .addValueEventListener(new ValueEventListener() {
 
@@ -84,12 +101,6 @@ public class Profile extends AppCompatActivity {
 
                 Log.i("List", "onDataChange: Execute");
                 mOutputText.setText(New[1]);
-//
-//                if (New[0].equals("false")){
-//                    Log.i("if Ex", "onCreate: if Exiciye");
-//                    mOutputText.setText(New[0] + " HI");
-//
-//                }
             }
 
             @Override
@@ -97,7 +108,6 @@ public class Profile extends AppCompatActivity {
 
             }
         });
-
 
 
 
@@ -150,11 +160,21 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode==REQUEST_CODE_FOR_IMAGEUPLOAD && resultCode==RESULT_OK && data!=null) {
+//            Uri uri = data.getData();
+//            CropImage.activity()
+//                    .setGuidelines(CropImageView.Guidelines.ON)
+//                    .setAspectRatio(1,1)
+//                    .setAllowRotation(true)
+//                    .start(this);
+//        }
         if (requestCode == REQUEST_CODE_FOR_IMAGEUPLOAD && resultCode == RESULT_OK && data!=null && data.getData()!=null){
             filePath=data.getData();
             Bitmap bitmap = null;
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
+                handleUpload(bitmap);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -172,6 +192,7 @@ public class Profile extends AppCompatActivity {
                             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                                 Toast.makeText(Profile.this, "Upload Successfully", Toast.LENGTH_SHORT).show();
                                 Log.i("which", "onSuccess: ");
+//                                getDownloadUrl(mStrRef);
 
                             }
                         })
@@ -185,10 +206,95 @@ public class Profile extends AppCompatActivity {
                         });
 
             }
-
         }
     }
+    private void handleUpload(Bitmap bitmap) {
 
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("profileImages")
+                .child(uid + ".jpeg");
+
+        reference.putBytes(baos.toByteArray())
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        getDownloadUrl(reference);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("which", "onFailure: ",e.getCause() );
+                    }
+                });
+    }
+
+//    private void getDownloadUrl(StorageReference mStrRef) {
+//        mStrRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//            @Override
+//            public void onSuccess(Uri uri) {
+//                setUserProfileUrl(uri);
+//            }
+//        });
+//    }
+private void getDownloadUrl(StorageReference reference) {
+    reference.getDownloadUrl()
+            .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Log.i("which", "onSuccess: " + uri);
+                    setUserProfileUrl(uri);
+                }
+            });
+}
+
+//    private void setUserProfileUrl(Uri uri) {
+//
+//        FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+//        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+//                .setPhotoUri(uri)
+//                .build();
+//
+//        user.updateProfile(request)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Toast.makeText(Profile.this, "Profile Update Successfully", Toast.LENGTH_SHORT).show();
+//
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//
+//                    }
+//                });
+//    }
+private void setUserProfileUrl(Uri uri) {
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+            .setPhotoUri(uri)
+            .build();
+
+    user.updateProfile(request)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(Profile.this, "Updated succesfully", Toast.LENGTH_SHORT).show();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(Profile.this, "Profile image failed...", Toast.LENGTH_SHORT).show();
+                }
+            });
+}
     private void ViewInstantProfile(View view) {
     }
 
